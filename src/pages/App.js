d1670efce1745a62720c9222885eb689ca460a04
Felipe de Araujo <felipe@youper.co>
2017-11-26 18:27:56 -0600

    Adds slash on root page

--- src/pages/App.js
+++ src/pages/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="" component={Layout} />
+        <Route path="/" component={Layout} />
       </BrowserRouter>
     );
   }
